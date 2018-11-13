635ca501594c0f023a5bafa8029e6f668b675f23
Daniel Yount <yountdaniel@gmail.com>
2018-11-12 22:50:38 -0500

    added a meta model for subreddit name/url etc...

---
+++ RedditRealTime/RedditRealTime/View Layer/View Controllers/SubredditSuggestionsPopOverViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SubredditSuggestionsPopOverViewController.swift
+//  RedditRealTime
+//
+//  Created by Daniel Yount on 11/12/18.
+//  Copyright © 2018 Daniel Yount. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SubredditSuggestionsPopOverViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        
+    }
+    
+}
