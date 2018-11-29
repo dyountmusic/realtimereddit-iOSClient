397d02f17459daa81593ac7d7ca492944acf13d0
Daniel Yount <yountdaniel@gmail.com>
2018-11-28 23:20:53 -0500

    added a user toggle option to prevent display from going to sleep.

--- RedditRealTime/RedditRealTime/Controller Layer/View Controllers/RealTimeSettingsTableViewController.swift
+++ RedditRealTime/RedditRealTime/Controller Layer/View Controllers/RealTimeSettingsTableViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class RealTimeSettingsTableViewController: UITableViewController {
 
     @IBOutlet weak var realTimeRefreshSwitch: UISwitch!
+    @IBOutlet weak var willDeviceSleepSwitch: UISwitch!
+    
     var redditModel = MetaRedditModel()
     
     var realTimeEnabled: Bool {
@@ -18,11 +20,27 @@ class RealTimeSettingsTableViewController: UITableViewController {
         set { UserDefaults.standard.set(newValue, forKey: "RealTimeEnabled") }
     }
     
+    var willDeviceSleep: Bool {
+        get { return UserDefaults.standard.bool(forKey: "DeviceSleepEnabled") }
+        set {
+            UserDefaults.standard.set(newValue, forKey: "DeviceSleepEnabled")
+            UIApplication.shared.isIdleTimerDisabled = newValue
+        }
+    }
+    
     fileprivate func checkForRealTime() {
         if realTimeEnabled {
             realTimeRefreshSwitch.isOn = true
         } else {
             realTimeRefreshSwitch.isOn = false
+        }
+    }
+    
+    fileprivate func checkForDeviceSleep() {
+        if willDeviceSleep {
+            willDeviceSleepSwitch.isOn = true
+        } else {
+            willDeviceSleepSwitch.isOn = false
         }
     }
     
@@ -36,7 +54,7 @@ class RealTimeSettingsTableViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
         
         checkForRealTime()
-        
+        checkForDeviceSleep()
     }
     
     @IBAction func realTimeRefreshSwitchTapped(_ sender: Any) {
@@ -47,72 +65,21 @@ class RealTimeSettingsTableViewController: UITableViewController {
         }
     }
     
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
+    @IBAction func willDeviceSleepSwitchTapped(_ sender: Any) {
+        if willDeviceSleepSwitch.isOn {
+            presentKeepDisplayOnAlert()
+            willDeviceSleep = true
+        } else {
+            willDeviceSleep = false
+        }
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 1
+    
+    func presentKeepDisplayOnAlert() {
+        let alert = UIAlertController(title: "Warning", message: "Leaving this option enabled for long periods of time can have an adverse effect on battery life and in rare cases cause display retention. Use at your own risk.", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        
+        self.present(alert, animated: true)
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
